c3c0da39ea2b96fe7f4984dc17078620191e4134
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2025-01-25 22:56:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from database import create_event_table, insert_event, get_all_events
 
 def create_event(name, date, time, location, description):
-    """Creates a new event in the database."""
+    
     insert_event(name, date, time, location, description)
     print(f"Event '{name}' created successfully!")
 
 def view_events():
-    """Displays all events from the database."""
     events = get_all_events()
     if events:
         print("\nUpcoming Events:")
